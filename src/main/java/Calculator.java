4c255708bb8fa586a57f14e112255d2a7e107de8
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-10-17 00:48:45 +0100

    Implemented subtract() method to pass test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
+    //Method for adding two integers
     public int add(int a, int b) {
         return a + b;
     }
+
+    //method for subtracting two integers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
 }
