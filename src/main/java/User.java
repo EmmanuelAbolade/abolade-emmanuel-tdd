c71d06f4e479beaabed1cedc0b3f0ce6328f4deb
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-10-24 08:59:19 +0100

    Added isPriority field and constructor to User for priority reservation support

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,12 +2,19 @@
 public class User {
     private String id;
     private String name;
+    private final boolean isPriority; //
 
     public User(String id, String name) {
         this.id = id;
         this.name = name;
+        this.isPriority = false; // default value
+    }
+    public User(String id, boolean isPriority) {
+        this.id = id;
+        this.isPriority = isPriority;
     }
 
     public String getId() { return id; }
     public String getName() { return name; }
+    public boolean isPriority() { return isPriority; }
 }
